042cb28638a90c31908d7e67cfc40d59d42a865f
zulfiqar <zulfiqarawan@gmail.com>
2023-01-03 18:14:41 +0000

    feat:jinja template added bulk uom conversion factor

--- erpnext/jinja.py
+++ erpnext/jinja.py
@@ -15,8 +15,10 @@ jenvs = {
 def trip_items(doc=None):
     values = {'sales_invoices': doc}
     res=frappe.db.sql("""
-                select item_name, stock_uom, sum(stock_qty) as qty from `tabSales Invoice Item`
-                where parent in %(sales_invoices)s 
-                group by item_name 
+                select i.item_name, i.stock_uom, conv.conversion_factor ,sum(i.stock_qty) as qty from `tabSales Invoice Item` as i
+                left join `tabUOM Conversion Detail` as conv
+                on conv.parent = i.item_code
+                where i.parent in %(sales_invoices)s and conv.uom ='Carton'
+                group by i.item_name 
                 """,values=values ,as_dict=True)
     return res
