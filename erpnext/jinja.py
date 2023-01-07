4c2adda3acdcc358ff76acb30f3823b59a5d70a8
zulfiqar <zulfiqarawan@gmail.com>
2023-01-07 10:23:50 +0000

    fix:jinja template function

--- erpnext/jinja.py
+++ erpnext/jinja.py
@@ -15,7 +15,10 @@ jenvs = {
 def trip_items(doc=None):
     values = {'sales_invoices': doc}
     res=frappe.db.sql("""
-                select i.item_name, i.stock_uom, conv.conversion_factor ,sum(i.stock_qty) as qty from `tabSales Invoice Item` as i
+                select i.item_name, i.stock_uom, 
+                min(conv.conversion_factor) as conversion_factor,
+                min(conv.uom) as bulk_uom
+                sum(i.stock_qty) as qty from `tabSales Invoice Item` as i
                 left join `tabUOM Conversion Detail` as conv
                 on conv.parent = i.item_code
                 where i.parent in %(sales_invoices)s and conv.conversion_factor > 1
