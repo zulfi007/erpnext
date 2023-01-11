1c22b1f49c9efa90a85cdbfd28e927c132e1ec78
zulfiqar <zulfiqarawan@gmail.com>
2023-01-11 09:34:43 +0000

    fix: jinja template

--- erpnext/jinja.py
+++ erpnext/jinja.py
@@ -21,7 +21,13 @@ def trip_items(doc=None):
                 sum(i.stock_qty) as qty from `tabSales Invoice Item` as i
                 left join `tabUOM Conversion Detail` as conv
                 on conv.parent = i.item_code
-                where i.parent in %(sales_invoices)s and conv.conversion_factor > 1
+                where i.parent in %(sales_invoices)s 
+                and 
+                    (
+                        conv.conversion_factor > 1
+                        OR conv.parent in
+                           (select parent from `tabUOM Conversion Detail` group by parent having count(*) = 1)
+                    )
                 group by i.item_name 
                 """,values=values ,as_dict=True)
     return res
